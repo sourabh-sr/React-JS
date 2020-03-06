8c4ecd3d87cd1168459fa952ae9c9dc3a964ed6a
Shadowfax7 <sourabh.srivatsa@shadowfax.in>
2020-03-06 12:35:46 +0530

    added prettier an eslint

---
+++ React Example 3/src/App.js
@@ -0,0 +1,49 @@
+/** This is bring in props */
+
+// const Wizard = (props) => {
+//     return React.createElement("div", {}, [
+//         React.createElement("h1", {}, props.name),
+//         React.createElement("h2", {}, props.wand),
+//         React.createElement("h2", {}, props.power)
+//     ])
+// }
+
+/**This is Destructuring of Props */
+
+const Wizard = ({
+  name,
+  wand,
+  power
+}) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, wand),
+    React.createElement("h2", {}, power)
+  ]);
+};
+
+const App = () => {
+  return React.createElement(
+    "div", {
+      id: "attributes"
+    },
+    React.createElement("h2", {}, "Welcome to Hogwards"),
+    React.createElement(Wizard, {
+      name: "HarryPotter",
+      wand: "Phoneix feather",
+      power: "Love"
+    }),
+    React.createElement(Wizard, {
+      name: "Voldemort",
+      wand: "Phoneix featther(Brother)",
+      power: "Ambition"
+    }),
+    React.createElement(Wizard, {
+      name: "Dumbeldore",
+      wand: "Elder Wand",
+      power: "Knowledge"
+    })
+  );
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
